228b439d8317d6d82e3fcee011018120122faa08
lucatorsello <luca.torsello47@thehutgroup.com>
2023-01-09 14:31:55 +0000

    first commit

--- src/main/java/com/thg/accelerator23/connectn/ai/jacklucajordan/HansNeimannsLittleSecret.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/jacklucajordan/HansNeimannsLittleSecret.java
@@ -1,24 +1,20 @@
 package com.thg.accelerator23.connectn.ai.jacklucajordan;
 
-
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
-import java.util.Random;
-
 
 public class HansNeimannsLittleSecret extends Player {
 
   public HansNeimannsLittleSecret(Counter counter) {
     super(counter, HansNeimannsLittleSecret.class.getName());
   }
-/Users/jackjenkinson/.m2/repository/com/github/THG-accelerator/connectn-player/1.4/connectn-player-1.4.jar!/com/thehutgroup/accelerator/connectn/player/Position.class
+
   @Override
   public int makeMove(Board board) {
-    //TODO: some crazy analysis
-    //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-
-    BoardAnalyser boardAnalyser = new BoardAnalyser(board);
+    // TODO: some crazy analysis
+    // TODO: make sure said analysis uses less than 2G of heap and returns within 10
+    // seconds on whichever machine is running it
 
     return 4;
   }
