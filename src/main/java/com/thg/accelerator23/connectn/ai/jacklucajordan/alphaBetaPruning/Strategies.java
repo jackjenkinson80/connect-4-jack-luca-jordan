b5b061688f249e128c967798bbf9facac064007a
lucatorsello <luca.torsello47@thehutgroup.com>
2023-01-11 13:13:25 +0000

    push for jack

--- src/main/java/com/thg/accelerator23/connectn/ai/jacklucajordan/alphaBetaPruning/Strategies.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/jacklucajordan/alphaBetaPruning/Strategies.java
@@ -2,37 +2,46 @@ package com.thg.accelerator23.connectn.ai.jacklucajordan.alphaBetaPruning;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.Position;
 import com.thg.accelerator23.connectn.ai.jacklucajordan.analysis.GameState;
 
 public class Strategies {
 
-    public static int strategyHub(GameState gameState, Board board) {
+    public static int strategyHub(GameState gameState, Board board, Counter nodeCounter) {
+        checkWinCondition(gameState);
+        for (int i = 0; i < board.getConfig().getWidth(); i++) {
+            for (int j = 0; j < board.getConfig().getHeight(); j++) {
+                Position currentPositionOnBoard = new Position(i, j);
+                Counter counterBeingParsedForEvaluation = board.getCounterAtPosition(currentPositionOnBoard);
+
+            }
+        }
         return checkWinCondition(gameState);
 
     }
 
     private static int checkWinCondition(GameState gameState) {
         if (gameState.isWin()) {
-            if (gameState.getWinner() == Counter.O) {
-                return 100;
-            } else {
-                return -100;
-            }
-        } else {
+            return 100;
+        } else
             return 0;
-        }
 
     }
 
-    private static int checkForTriplets(Board board) {
+    private static int moveInMiddleRowIfIsGoodMove(Board board, Counter targetCounter) {
         return 1;
     }
 
-    private static int checkForDuplets(Board board) {
+    private static int checkForTriplets(Board board, Counter targetCounter) {
         return 1;
     }
 
-    private static int checkForForks(Board board) {
+    private static int checkForDuplets(Board board, Counter targetCounter) {
         return 1;
     }
+
+    private static int checkForForks(Board board, Counter targetCounter) {
+        return 1;
+    }
+
 }
